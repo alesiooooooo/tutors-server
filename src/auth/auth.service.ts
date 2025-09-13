ec30f66d9cca28a9a8aeaeb43a4f2c48c92b3641
alesiooooooo <69792682+alesiooooooo@users.noreply.github.com>
2025-09-13 10:32:11 +0300

    process:  implemented login signup methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from 'src/user/user.entity';
 import { AuthDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-  login(dto: AuthDto) {
-    throw new Error('Method not implemented.');
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signup(dto: AuthDto) {
+    const exist = await this.userRepo.findOne({ where: { email: dto.email } });
+    if (exist) {
+      throw new BadRequestException('Email already taken');
+    }
+
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const user = this.userRepo.create({
+      email: dto.email,
+      password: passwordHash,
+    });
+
+    await this.userRepo.save(user);
+
+    return { message: 'User created successfully' };
   }
 
-  signup(dto: AuthDto) {
-    throw new Error('Method not implemented.');
+  async login(dto: AuthDto) {
+    const user = await this.userRepo.findOne({ where: { email: dto.email } });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isMatch = await bcrypt.compare(dto.password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { userId: user.id, email: user.email };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { access_token: token };
   }
 }

b5ac0168c34f6339aa854e78caef76557c656286
alesiooooooo <69792682+alesiooooooo@users.noreply.github.com>
2025-09-13 09:16:00 +0300

    ADDED: seeding tutors

--- src/tutor/tutor.module.ts
+++ src/tutor/tutor.module.ts
@@ -1,12 +1,30 @@
+import { OnModuleInit } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { TutorService } from './tutor.service';
 import { TutorController } from './tutor.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
 import { Tutor } from './tutor.entity';
+import { Repository } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tutor])],
   providers: [TutorService],
   controllers: [TutorController],
 })
-export class TutorModule {}
+export class TutorModule implements OnModuleInit {
+  constructor(
+    @InjectRepository(Tutor)
+    private tutorRepo: Repository<Tutor>,
+  ) {}
+
+  async onModuleInit() {
+    const count = await this.tutorRepo.count();
+    if (count === 0) {
+      await this.tutorRepo.save([
+        this.tutorRepo.create({ name: 'Math teacher' }),
+        this.tutorRepo.create({ name: 'English teacher' }),
+        this.tutorRepo.create({ name: 'Physics teacher' }),
+      ]);
+    }
+  }
+}
